453f1bbbe29b61f54048a15f82642ba5beee1d5c
cjsrhd94 <cjsrhd94@gmail.com>
2023-02-07 11:32:46 +0900

    feat: Comment 엔티티 생성

---
+++ src/main/java/com/test/demo/entity/Comment.java
@@ -0,0 +1,41 @@
+package com.test.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.Where;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Where(clause = "deleted = false")
+public class Comment {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String content;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "post_id")
+	private Post post;
+
+	@Builder
+	public Comment(String content) {
+		this.content = content;
+	}
+}
