191c5466dc99caa1efee2ad6b1418da7bf8dec35
cjsrhd94 <cjsrhd94@gmail.com>
2023-02-10 14:33:15 +0900

    fix: AsyncConfig 내 @Configuration 추가

--- src/main/java/com/test/demo/global/AsyncConfig.java
+++ src/main/java/com/test/demo/global/AsyncConfig.java
@@ -1,7 +1,9 @@
 package com.test.demo.global;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
+@Configuration
 public class AsyncConfig {
 }
